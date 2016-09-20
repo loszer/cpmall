722a28b0586e348613e967654273fac631065b75
garan <loszer@163.com>
2016-09-21 01:04:49 +0800

    regular commit

--- business.py
+++ business.py
@@ -1,9 +1,12 @@
 import web
 import datetime
+import json
+import sqlite3
+import random
 
 db = web.database(dbn='mysql', user='root', pw='', db='cpmall')
 
-def test(clientTime):
+def getCurrentPhase(clientTime):
     if not db:
         return 'db open error'
     pass
@@ -11,14 +14,9 @@ def test(clientTime):
     if first_rec['kaipantime'].date() != datetime.date.today():
         # two time not match, change the date of datetime in the sql
         _changeDateTimeAll(clientTime)
+        return 'change the datetime, please visit once again'
     _changeStatus(clientTime)
     return db.query("SELECT * FROM cp_kaipanbjpk10 WHERE status = 2 ORDER BY phase LIMIT 1")[0]
-
-# def getCurrentPhase():
-#     if _getSpecificPhaseByStatus()['phase'] != _getSpecficPhaseByDateTime(datetime.datetime.now())['phase']:
-#         _changeStatus()
-#         pass
-#     return _getSpecificPhaseByStatus()
 
 def _getSpecificPhaseByStatus(status=2):
     # get the phase whose status = 2
@@ -50,7 +48,7 @@ def _changeStatus(currentTime):
         # the line is no meaning
         pass
     elif currentTime >= fengpanTime:
-        db.query("UPDATE cp_kaipanbjpk10 SET status = 3  WHERE fengpantime <= '"+currentTime.strftime("%Y-%m-%d %H:%M:%S")+"' AND status = 2")
+        db.query("UPDATE cp_kaipanbjpk10 SET status = 3  WHERE fengpantime <= '"+currentTime.strftime("%Y-%m-%d %H:%M:%S")+"' ")
         tmp = _getSpecficPhaseByDateTime(currentTime)['phase']
         db.query("UPDATE cp_kaipanbjpk10 SET status = 2  WHERE phase ="+str(tmp)+" ORDER BY phase DESC LIMIT 1")
     pass
@@ -64,3 +62,49 @@ def _changeDateTimeAll(currentTime):
         kaijiangtime = datetime.datetime.combine(now.date(),i['kaijiangtime'].time()).strftime("%Y-%m-%d %H:%M:%S")
         db.query("UPDATE cp_kaipanbjpk10 SET kaipantime = '"+kaipantime+"', fengpantime = '"+fengpantime+"', kaijiangtime = '"+kaijiangtime+"' WHERE id = "+str(i['id'])+" AND phase= "+str(i['phase'])+"")
     pass
+
+def getBetsAsJsonStr():
+    fp = open('./mock/db.json')
+    content = fp.read()
+    fp.close()
+    content = json.loads(content)
+    # make a deal with the actual bets , such as making them random
+    # ...
+    return content['bets'][0]
+
+def addUser(username,password):
+    conn = sqlite3.connect('./mock/data.db')
+    c= conn.cursor()
+    res = c.execute("select * from user where username = '"+username+"'").fetchall()
+    if len(res) != 0:
+        # exist the same username
+        return 0
+    # hack code , not safe, no check the exception
+    c.execute("insert into user (username, password) values ('"+username+"', '"+password+"')")
+    conn.commit()
+    conn.close()
+    return 1
+
+def getUser(username, password):
+    conn = sqlite3.connect('./mock/data.db')
+    c = conn.cursor()
+    res = c.execute("select balance, bill from user where username = '" + username + "' and password = '"+password+"'").fetchall()
+    if len(res) == 1:
+        if res[0][0] and res[0][1]:
+            pass
+        else:
+            if res[0][0] == None: c.execute("update user set balance = "+str(round(random.uniform(1000, 10000), 2))+" where username = '"+username+"'")
+            if res[0][1] == None: c.execute("update user set bill = "+str(round(random.uniform(50, 2000), 2))+" where username = '"+username+"'")
+            conn.commit()
+        res = c.execute("select balance, bill from user where username = '" + username + "'").fetchall()[0]
+        return {'username': username, 'balance': res[0], 'bill': res[1]}
+    return 0
+
+def login(username, password):
+    conn = sqlite3.connect('./mock/data.db')
+    c = conn.cursor()
+    res = c.execute("select * from user where username = '" + username + "' and password = '" + password + "'").fetchall()
+    conn.close()
+    if len(res) == 0:
+        return 0
+    return 1
--- code.py
+++ code.py
@@ -2,20 +2,88 @@ import web
 import datetime
 import time
 import json
+import random
 
 import business
 
 urls = (
-    '/(.*)', 'hello'
+    '/test', 'test',
+    '/hello', 'hello',
+    '/phases', 'phase',
+    '/bets', 'bet',
+    '/users', 'user',
+    '/login', 'login'
 )
 app = web.application(urls, globals())
 
 
+class test:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        return web.input()['name']
+    def POST(self):
+        return web.input()['name']
+
 class hello:
-    def GET(self, name):
-        if not name:
-            name = "world"
-        return "hello "+name+"!"
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        return business.getCurrentPhase(datetime.datetime.now())
+
+class phase:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', "text/json")
+        res =  business.getCurrentPhase(datetime.datetime.now())
+        return json.dumps({'status_code': 200, 'msg': 'get the phase success', 'data':{'id': res['id'], 'phase': res['phase']}})
+
+class bet:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', "text/json")
+        bets = business.getBetsAsJsonStr()
+        # return json.dumps(bets)
+        for  i in range(len(bets['data'])):
+            for k in bets['data'][i].keys():
+                if 'position' in k:
+                    continue
+                bets['data'][i][k] = round(random.uniform(0.5, 2), random.randint(2, 4))
+        # bets['phase'] = business.getCurrentPhase(datetime.datetime.now())['phase']
+        return json.dumps(bets)
+
+class user:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'text/json')
+        if not (web.input().has_key('username') and web.input().has_key('password')):
+            return json.dumps({'status_code': 1001, 'msg': 'lack the necessary param'})
+        res = business.getUser(web.input()['username'], web.input()['password'])
+        if not res:
+            return json.dumps({'status_code': 100, 'msg': 'no these user'})
+        return json.dumps({'status_code': 200, 'msg': 'get the user info without password', 'data': res})
+    def POST(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'text/json')
+        user_data = web.input()
+        if not business.addUser(user_data['username'],user_data['password']):
+            return json.dumps({'status': 'error', 'msg': 'some error'})
+        return  json.dumps({'status': 'success', 'msg': 'logon success'})
+    def PUT(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        return 'put'
+    def DELETE(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        return 'delete'
+
+class login:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'text/json')
+        # check the username and passworld filed not empty
+        # ...
+        user_data = web.input()
+        if business.login(user_data['username'], user_data['password']):
+            return json.dumps({"status_code": 200})
+        return json.dumps({"status_code": 100, "msg": "login failed"})
 
 if __name__ == "__main__":
     app.run()
